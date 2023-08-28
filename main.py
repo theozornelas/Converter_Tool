b0c4a89472ce60a080d874b18fee7a17b5222474
osvaldo  moreno ornelas <osvaldomorenoornelas@osvaldos-MacBook-Air.local>
2023-08-27 20:41:11 -0700

    added initial module includes and skeleton main

---
+++ main.py
@@ -0,0 +1,8 @@
+import ebooklib
+from ebooklib import epub
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Hola")
